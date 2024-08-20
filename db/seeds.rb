822190240a4f221ae07a4663bf73496ee4c5e5e1
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-08-20 11:49:14 +0200

    seeds back

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,42 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-# puts "Deleting any existing restaurants..."
-# User.destroy_all
-# Service.destroy_all
+puts "Deleting any existing restaurants..."
+User.destroy_all
+Service.destroy_all
 
-# frida = User.create!(first_name: "Frida",
-#   last_name: "Schuh",
-#   username: "FridaDaCook",
-#   password:"123",
-#   email: "hireme@bookthecooks.com"
-# )
+frida = User.create!(first_name: "Frida",
+  last_name: "Schuh",
+  username: "FridaDaCook",
+  password:"123",
+  email: "hireme@bookthecooks.com"
+)
 
-# Service.create!(name: "Schnitzel in your face",
-#   category: "German",
-#   user: frida.id
-# )
+Service.create!(name: "Schnitzel in your face",
+  category: "German",
+  user: frida.id
+)
 
-# lounis = User.create!(first_name: "Lounis",
-#   last_name: "Boudjemia",
-#   username: "TheLounis",
-#   password:"lounis",
-#   email: "lounisforhire@bookthecooks.com"
-# )
+lounis = User.create!(first_name: "Lounis",
+  last_name: "Boudjemia",
+  username: "TheLounis",
+  password:"lounis",
+  email: "lounisforhire@bookthecooks.com"
+)
 
-# Service.create!(name: "Good food for you",
-#   category: "French",
-#   user: lounis.id
-# )
+Service.create!(name: "Good food for you",
+  category: "French",
+  user: lounis.id
+)
 
-# arnold = User.create!(first_name: "Arnold",
-#   last_name: "Schwarzenegger",
-#   username: "TheTerminator",
-#   password:"Illbeback",
-#   email: "getothechoppanow@predator.com"
-# )
+arnold = User.create!(first_name: "Arnold",
+  last_name: "Schwarzenegger",
+  username: "TheTerminator",
+  password:"Illbeback",
+  email: "getothechoppanow@predator.com"
+)
 
-# Service.create!(name: "Stuff your fat face",
-#   category: "Fried Chicken",
-#   user: arnold.id
-# )
+Service.create!(name: "Stuff your fat face",
+  category: "Fried Chicken",
+  user: arnold.id
+)
