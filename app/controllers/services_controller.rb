badd6587babce9972190730b7f6532eacab15e4e
Frida Schuch <fridaschuch@googlemail.com>
2024-08-20 12:45:30 +0200

    added methods edit and update in services controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -16,6 +16,19 @@ class ServicesController < ApplicationController
     @service = Service.save
   end
 
+  def edit
+    @service = Service.find(params[:id])
+  end
+
+  def update
+    @service = Service.find(params[:id])
+    if @service.update(service_params)
+      redirect_to @service, notice: 'You updated your Service successfully.'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def service_params
