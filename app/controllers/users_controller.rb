5befd1d3317f6d36ef2964eec8072040f124dd94
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2024-08-20 18:06:15 +0200

    added booking seeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  def show
+  def profile
     @user = current_user
     @my_bookings = Booking.all.select { |m| m.user_id == @user.id }
+    @my_gigs = Service.all.select { |m| m.user_id == @user.id }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :users, only: [:show]
+  get "profile", to: "users#profile"
 end
