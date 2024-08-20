b3a53818129de1a71c5e0c1ea105554c42ad2446
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-08-20 11:43:27 +0200

    services index.html setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,54 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-puts "Deleting any existing restaurants..."
-User.destroy_all
-Service.destroy_all
+# puts "Deleting any existing restaurants..."
+# User.destroy_all
+# Service.destroy_all
 
-arnold = User.create!(first_name: "Arnold",
-  last_name: "Schwarzenegger",
-  username: "TheTerminator",
-  password:"Illbeback",
-  email: "getothechoppanow@predator.com"
-)
+# frida = User.create!(first_name: "Frida",
+#   last_name: "Schuh",
+#   username: "FridaDaCook",
+#   password:"123",
+#   email: "hireme@bookthecooks.com"
+# )
 
-Service.create!(name: "Stuff your fat face",
-  category: "Fried Chicken",
-  user: arnold.id
-)
+# Service.create!(name: "Schnitzel in your face",
+#   category: "German",
+#   user: frida.id
+# )
 
-arnold = User.create!(first_name: "Arnold",
-  last_name: "Schwarzenegger",
-  username: "TheTerminator",
-  password:"Illbeback",
-  email: "getothechoppanow@predator.com"
-)
+# lounis = User.create!(first_name: "Lounis",
+#   last_name: "Boudjemia",
+#   username: "TheLounis",
+#   password:"lounis",
+#   email: "lounisforhire@bookthecooks.com"
+# )
 
-Service.create!(name: "Stuff your fat face",
-  category: "Fried Chicken",
-  user: arnold.id
-)
+# Service.create!(name: "Good food for you",
+#   category: "French",
+#   user: lounis.id
+# )
 
-arnold = User.create!(first_name: "Arnold",
-  last_name: "Schwarzenegger",
-  username: "TheTerminator",
-  password:"Illbeback",
-  email: "getothechoppanow@predator.com"
-)
+# arnold = User.create!(first_name: "Arnold",
+#   last_name: "Schwarzenegger",
+#   username: "TheTerminator",
+#   password:"Illbeback",
+#   email: "getothechoppanow@predator.com"
+# )
 
-Service.create!(name: "Stuff your fat face",
-  category: "Fried Chicken",
-  user: arnold.id
-)
-
-arnold = User.create!(first_name: "Arnold",
-  last_name: "Schwarzenegger",
-  username: "TheTerminator",
-  password:"Illbeback",
-  email: "getothechoppanow@predator.com"
-)
-
-Service.create!(name: "Stuff your fat face",
-  category: "Fried Chicken",
-  user: arnold.id
-)
+# Service.create!(name: "Stuff your fat face",
+#   category: "Fried Chicken",
+#   user: arnold.id
+# )
