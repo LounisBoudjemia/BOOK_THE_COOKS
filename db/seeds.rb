266ae72fc728b192906a4166d7915bad8e4ade37
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-08-20 11:21:15 +0200

    created a new user and service

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,53 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 puts "Deleting any existing restaurants..."
+User.destroy_all
 Service.destroy_all
+
+arnold = User.create!(first_name: "Arnold",
+  last_name: "Schwarzenegger",
+  username: "TheTerminator",
+  password:"Illbeback",
+  email: "getothechoppanow@predator.com"
+)
 
 Service.create!(name: "Stuff your fat face",
   category: "Fried Chicken",
+  user: arnold.id
 )
 
-Service.create!(name: "Oh look! Food!",
-  category: "Chinese",
+arnold = User.create!(first_name: "Arnold",
+  last_name: "Schwarzenegger",
+  username: "TheTerminator",
+  password:"Illbeback",
+  email: "getothechoppanow@predator.com"
 )
 
+Service.create!(name: "Stuff your fat face",
+  category: "Fried Chicken",
+  user: arnold.id
+)
 
-Service.create!(name: "Bob's Burgers",
-  category: "Burgers",
+arnold = User.create!(first_name: "Arnold",
+  last_name: "Schwarzenegger",
+  username: "TheTerminator",
+  password:"Illbeback",
+  email: "getothechoppanow@predator.com"
+)
+
+Service.create!(name: "Stuff your fat face",
+  category: "Fried Chicken",
+  user: arnold.id
+)
+
+arnold = User.create!(first_name: "Arnold",
+  last_name: "Schwarzenegger",
+  username: "TheTerminator",
+  password:"Illbeback",
+  email: "getothechoppanow@predator.com"
+)
+
+Service.create!(name: "Stuff your fat face",
+  category: "Fried Chicken",
+  user: arnold.id
 )
