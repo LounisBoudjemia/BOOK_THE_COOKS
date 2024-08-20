b8833cedc7ad9c19dc829d3bb71b82b635cd0e95
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2024-08-20 12:31:57 +0200

    added booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,8 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @user = current_user
+    @service = Service.first # needs to be changed
   end
 
   def create
