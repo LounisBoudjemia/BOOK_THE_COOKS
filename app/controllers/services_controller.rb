8a3fa76798d3e976054eff1012314fc5751b46e0
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-08-20 15:36:02 +0200

    refactored services controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,10 +1,11 @@
 class ServicesController < ApplicationController
+  before_action :set_service, only: [:show, :edit, :update, :destroy]
   def index
     @services = Service.all
   end
 
   def show
-    @service = Service.find(params[:id])
+   # @service = Service.find(params[:id])
     @user = User.new(last_name: "Schuch", first_name: "Frida")
   end
 
@@ -24,11 +25,11 @@ class ServicesController < ApplicationController
   end
 
   def edit
-    @service = Service.find(params[:id])
+   # @service = Service.find(params[:id])
   end
 
   def update
-    @service = Service.find(params[:id])
+   # @service = Service.find(params[:id])
     if @service.update(service_params)
       redirect_to @service, notice: 'You updated your Service successfully.'
     else
@@ -45,6 +46,10 @@ class ServicesController < ApplicationController
   private
 
   def service_params
-    params.require(:service).permit(:name, :category, :description)
+    params.require(:service).permit(:name, :category, :description, :photo)
+  end
+
+  def set_service
+    @service = Service.find(params[:id])
   end
 end
