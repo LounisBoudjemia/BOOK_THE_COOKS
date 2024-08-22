d462c3820812a17146f8f551dcf484a5285df83c
Lounis Boudjemia <lounisboudjemia@gmail.com>
2024-08-22 10:59:32 +0200

    fixed photo issues

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,7 @@ class Service < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
+  #has_many_attached :photos
 
   validates :name, presence: true
   validates :category, presence: true
