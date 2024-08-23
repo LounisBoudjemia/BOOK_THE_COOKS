8a0dc62699bf310ac942247b83b9ef4063b3ce8f
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2024-08-23 16:04:10 +0200

    cards are used through shared partial, link to service added from bookings and gigs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,10 @@ class UsersController < ApplicationController
     # select all bookings that belong to the current user
     @my_bookings = Booking.all.select { |booking| booking.user_id == @user.id }
     # select all services that belong to the current user
-    @my_services = Service.all.select { |service| service.user_id == @user.id }
+    @services = Service.all.select { |service| service.user_id == @user.id }
     # select all service ids that belong to the current user
-    @my_service_ids = @my_services.map { |service| service[:id] }
+    @service_ids = @services.map { |service| service[:id] }
     # select all bookings that belong to these services
-    @my_gigs = Booking.where(service_id: @my_service_ids)
+    @my_gigs = Booking.where(service_id: @service_ids)
   end
 end
